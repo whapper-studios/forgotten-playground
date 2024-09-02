333cf61ca17a55aaf05f382200a94b62ee10b069
kenanoestreich <kenano1999@gmail.com>
2024-09-02 12:30:19 -0500

    feat: add animations to masks

--- Masks.cs
+++ Masks.cs
@@ -4,15 +4,12 @@ using Godot;
 public partial class Masks : HBoxContainer
 {
 
-	private string pathToFullMask = "[img]assets/Mask.png[/img]";
-	private string pathToEmptyMask = "[img]assets/MaskEmpty.png[/img]";
 	private int totalMasks = 5;
 	private int currentMasks;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		currentMasks = totalMasks;
-		UpdateMasks();
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -23,24 +20,25 @@ public partial class Masks : HBoxContainer
 	// Called when HK is hit by an enemy, and triggers short animation of mask breaking.
 	public void RemoveMask()
 	{
+		GetNode<AnimatedSprite2D>($"CenterContainer{currentMasks}/AnimatedSprite2D").Play(Name="MaskBreak");
 		currentMasks -= 1;
-		UpdateMasks();
 	}
 
-	public void UpdateMasks()
+	private void AddMask()
 	{
-		for (int i = 0; i<totalMasks; i++)
-		{
-			
-			if (i<currentMasks)
-			{
-				GetNode<RichTextLabel>($"Mask{i}").Text = pathToFullMask;
-			}
-			else 
-			{
-				GetNode<RichTextLabel>($"Mask{i}").Text = pathToEmptyMask;
-			}
-		}
-
+		currentMasks += 1;
+		GetNode<AnimatedSprite2D>($"CenterContainer{currentMasks}/AnimatedSprite2D").PlayBackwards(Name="MaskBreak");
 	}
+
+    public override void _GuiInput(InputEvent @event)
+    {
+        if (@event.IsActionReleased("select") && currentMasks != 0)
+		{
+			RemoveMask();
+		}
+		if (@event.IsActionReleased("secondary") && currentMasks != totalMasks)
+		{
+			AddMask();
+		}
+    }
 }
