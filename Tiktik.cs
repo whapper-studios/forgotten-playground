89f7ab9019baa56ad962f9948f130a60f1b95418
retsofoster <elijah.foster@live.com>
2024-09-04 12:21:24 -0400

    Updated to allow for easy placement of Tiktik onto level

--- Tiktik.cs
+++ Tiktik.cs
@@ -4,6 +4,7 @@ using System.Globalization;
 //using System.Numerics;
 
 using System.Reflection.Metadata.Ecma335;
+using System.Runtime.ExceptionServices;
 
 
 public partial class Tiktik : CharacterBody2D
@@ -12,24 +13,25 @@ public partial class Tiktik : CharacterBody2D
 	[Export] RayCast2D Front;
 	[Export] AnimatedSprite2D running;
 	public const float Speed = 150.0f;
+	public const float Gravity = 500f;
 
-	public Vector2 currentbasis = new Vector2();
-	public bool test = false;
-	[Export] public Timer timer;
-
-	public int num = 0;
-	public int ran = 0; 
+	public int rotatedNum = 0; 
 	public bool isRotating = false;
+	public bool firsttouch = false;
 	public bool shouldRotate = false;
     public override void _Ready()
     {
-       timer.Start();
 	   running.Play();
     }
     public override void _PhysicsProcess(double delta)
 	{
-		if(timer.IsStopped())
+		if(!IsOnFloor() && firsttouch == false)
 		{
+			ApplyGravity((float)delta);
+		}
+		else
+		{
+			firsttouch = true;
 			if(!isRotating)
 			{
 				if(Back.IsColliding() || Front.IsColliding())
@@ -41,39 +43,48 @@ public partial class Tiktik : CharacterBody2D
 					shouldRotate = true;
 				}
 			}
+			
 			if(shouldRotate)
 			{
 				isRotating = true;
-				if(num <= 11)
+				if(rotatedNum <= 11)
 				{
 					Rotate();
 				}
 				else
 				{
+
 					isRotating = false;
 					shouldRotate = false;
-					num = 0;
+					rotatedNum = 0;
 				}
+				
 			}
 		}
 	}
 	
+	private void ApplyGravity(float delta)
+	{
+		Vector2 upDirection = new Vector2(0,-1).Rotated(Rotation);
+		this.SetUpDirection(upDirection);
+		Velocity -= delta * upDirection * Gravity;
+		MoveAndCollide(Velocity);
+		Velocity = new Vector2(0,0);
+		MoveAndSlide();
+	}
 
     private void Move()
     {
 		Vector2 forward = new Vector2(Mathf.Cos(Rotation), Mathf.Sin(Rotation));
-        Velocity = forward * Speed;
-
-        MoveAndSlide();
-		ran++;
+		Velocity = forward * Speed;
+        
+		MoveAndSlide();
 	}
-
+	
 	private void Rotate()
 	{
 		this.RotationDegrees += 7.5f;
-		num++;
+		rotatedNum++;
 	}
-
-
 }
 
