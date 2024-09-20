2bb5b93addb6e5d7c508b1f8f8564ac81f5ec67a
kenanoestreich <kenano1999@gmail.com>
2024-09-19 19:50:37 -0500

    feat: start geo animation 1.5 sec after new geo

--- ui/GeoCounter.cs
+++ ui/GeoCounter.cs
@@ -1,9 +1,13 @@
 using Godot;
 using System;
+using System.Data;
 
-public partial class GeoCounter : RichTextLabel
+public partial class GeoCounter : Control
 {
 	
+	private bool isGeoTransferring = false;
+	private int geoAdded = 0;
+	private int geoTotal = 0;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -13,4 +17,35 @@ public partial class GeoCounter : RichTextLabel
 	public override void _Process(double delta)
 	{
 	}
+
+	// Updates the counter on GeoAdded node, and, after a short time, decreases the GeoAdded count 
+	// while increasing the CurrentGeo count. While the geo is "transferring", plays animation on GeoIcon
+	public void UpdateGeoAdded(int newGeo)
+	{
+		if (geoAdded == 0)
+		{
+			GetNode<RichTextLabel>("HBoxContainer/VBoxContainer/Control/PlusSign").Visible = true;
+		}
+		RichTextLabel addedGeoNode = GetNode<RichTextLabel>("HBoxContainer/VBoxContainer2/CenterContainer2/AddedGeo");
+		addedGeoNode.Text = (geoAdded+newGeo).ToString();
+		geoAdded += newGeo;
+		addedGeoNode.GetChild<Timer>(0).Stop();
+		addedGeoNode.GetChild<Timer>(0).Start();
+		
+	}
+
+	public void OnTimerTimeout()
+	{
+		GetNode<AnimatedSprite2D>("HBoxContainer/VBoxContainer/CenterContainer/Control/GeoIcon").Play();
+	}
+
+	
+    //
+    public override void _GuiInput(InputEvent @event)
+    {
+        if (@event.IsActionPressed("select"))
+		{
+			UpdateGeoAdded(1);
+		}
+    }
 }
